898980712f763df53baf6beb5040dc6577cdd9db
SaumilBisht <bishtsaumil@gmail.com>
2024-09-27 07:32:06 +0530

    save cart in local storage

--- data/cart.js
+++ data/cart.js
@@ -1,10 +1,18 @@
-export let cart=[{
-  productId:"e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-  quantity:2
-},{
-  productId:"15b6fc6f-327a-4ec4-896f-486349e85a3d",
-  quantity:1
-}];
+export let cart= JSON.parse(localStorage.getItem('cart')); //purana cart uthayega jo saved h cuz pura js firse chlta h
+if(!cart){
+  cart=[{
+    productId:"e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    quantity:2
+  },{
+    productId:"15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    quantity:1
+  }];
+}
+
+
+function saveToStorage(){
+  localStorage.setItem('cart',JSON.stringify(cart));
+}// cart ko local storage mein save so that refresh krne ke baad wapis default top wli cart na ajae
 export function addToCart(productId){
   let matchingItem;
   
@@ -23,6 +31,8 @@ export function addToCart(productId){
       quantity:1
     });
   }
+
+  saveToStorage();
   
 };
 
@@ -34,4 +44,6 @@ export function removeFromCart(productId){
     }
   });
   cart=newCart;
+
+  saveToStorage();
 }
