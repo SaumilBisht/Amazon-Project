2439cb975f71c7c11631bceb3c506d5f89cb7a0c
SaumilBisht <bishtsaumil@gmail.com>
2024-10-16 12:18:01 +0530

    cart working fine now

--- data/cart.js
+++ data/cart.js
@@ -98,29 +98,3 @@ export function updateDeliveryOption(productId,deliveryOptionId)
 
   saveToStorage();
 }
-
-
-
-export function calculateCartQuantity(){ // amazon and checkout page dono ke liye
-  let cartQuantity = 0;
-
-  cart.forEach((cartItem) => {
-    cartQuantity += cartItem.quantity;
-  });
-  
-  return cartQuantity;
-}
-
-export function updateQuantity(productId, newQuantity) {
-  let matchingItem;
-
-  cart.forEach((cartItem) => {
-    if (productId === cartItem.productId) {
-      matchingItem = cartItem;
-    }
-  });
-
-  matchingItem.quantity = newQuantity;
-
-  saveToStorage();
-}
