c13cac3b2be55c54618bb6d6265694141a8006eb
SaumilBisht <bishtsaumil@gmail.com>
2024-10-16 12:13:49 +0530

    cart thik

--- data/cart.js
+++ data/cart.js
@@ -2,17 +2,18 @@ export let cart;
 
 loadFromStorage();
 
-export function loadFromStorage(){
-  cart= JSON.parse(localStorage.getItem('cart')); //purana cart uthayega jo saved h cuz pura js firse chlta h
-  if(!cart){
-    cart=[{
-      productId:"e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-      quantity:2,
-      deleviryOptionId: '1'
-    },{
-      productId:"15b6fc6f-327a-4ec4-896f-486349e85a3d",
-      quantity:1,
-      deleviryOptionId: '2'
+export function loadFromStorage() {
+  cart = JSON.parse(localStorage.getItem('cart'));
+
+  if (!cart) {
+    cart = [{
+      productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+      quantity: 2,
+      deliveryOptionId: '1'
+    }, {
+      productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+      quantity: 1,
+      deliveryOptionId: '2'
     }];
   }
 }
@@ -21,6 +22,7 @@ export function loadFromStorage(){
 function saveToStorage(){
   localStorage.setItem('cart',JSON.stringify(cart));
 }// cart ko local storage mein save so that refresh krne ke baad wapis default top wli cart na ajae
+
 export function addToCart(productId){
   let matchingItem;
   const quantitySelector=document.querySelector(`.js-quantity-selector-${productId}`);
@@ -93,6 +95,32 @@ export function updateDeliveryOption(productId,deliveryOptionId)
   });
 
   matchingItem.deliveryOptionId=deliveryOptionId;
+
+  saveToStorage();
+}
+
+
+
+export function calculateCartQuantity(){ // amazon and checkout page dono ke liye
+  let cartQuantity = 0;
+
+  cart.forEach((cartItem) => {
+    cartQuantity += cartItem.quantity;
+  });
+  
+  return cartQuantity;
+}
+
+export function updateQuantity(productId, newQuantity) {
+  let matchingItem;
+
+  cart.forEach((cartItem) => {
+    if (productId === cartItem.productId) {
+      matchingItem = cartItem;
+    }
+  });
+
+  matchingItem.quantity = newQuantity;
 
   saveToStorage();
 }
