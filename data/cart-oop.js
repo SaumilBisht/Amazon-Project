bc37aa673672d9a70aaa7c183bc5eb5f270a587e
SaumilBisht <bishtsaumil@gmail.com>
2024-10-17 12:05:03 +0530

    create oop version of cart

---
+++ data/cart-oop.js
@@ -0,0 +1,119 @@
+function Cart(localStorageKey){
+  const cart={
+    cartItems:undefined,
+  
+    loadFromStorage() {
+      this.cartItems = JSON.parse(localStorage.getItem(localStorageKey));
+    
+      if (!this.cartItems) {
+        this.cartItems = [{
+          productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+          quantity: 2,
+          deliveryOptionId: '1'
+        }, {
+          productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+          quantity: 1,
+          deliveryOptionId: '2'
+        }];
+      }
+    },
+  
+    saveToStorage(){
+      localStorage.setItem(localStorageKey,JSON.stringify(this.cartItems));
+    },
+  
+    addToCart(productId){
+      let matchingItem;
+      const quantitySelector=document.querySelector(`.js-quantity-selector-${productId}`);
+      const quantity=Number(quantitySelector.value);
+      this.cartItems.forEach((cartItem)=>{
+        if(cartItem.productId===productId) matchingItem=cartItem;
+      });
+    
+      if(matchingItem)
+      {
+        matchingItem.quantity+=quantity;  //if already present AND by default dom se string aata isliye number convert 
+      }
+      else
+      {
+        this.cartItems.push({
+          productId: productId,
+          quantity:quantity, // jitni quantity select kiya h
+          deliveryOptionId: '1'
+        });
+      }
+    
+      this.saveToStorage();
+    },
+  
+    removeFromCart(productId){
+      let newCart=[];
+      this.cartItems.forEach((item)=>{
+        if(item.productId!==productId){
+          newCart.push(item);
+        }
+      });
+      this.cartItems=newCart;
+    
+      this.saveToStorage();
+    },
+  
+    updateDeliveryOption(productId,deliveryOptionId)
+    {
+      let matchingItem;
+  
+      this.cartItems.forEach((cartItem) => {
+        if (productId === cartItem.productId) {
+          matchingItem = cartItem;
+        }
+      });
+  
+      matchingItem.deliveryOptionId=deliveryOptionId;
+  
+      this.saveToStorage();
+    },
+  
+    calculateCartQuantity(){ // amazon and checkout page dono ke liye
+      let cartQuantity = 0;
+    
+      this.cartItems.forEach((cartItem) => {
+        cartQuantity += cartItem.quantity;
+      });
+      
+      return cartQuantity;
+    },
+  
+    updateQuantity(productId, newQuantity) {
+      let matchingItem;
+    
+      this.cartItems.forEach((cartItem) => {
+        if (productId === cartItem.productId) {
+          matchingItem = cartItem;
+        }
+      });
+    
+      matchingItem.quantity = newQuantity;
+    
+      this.saveToStorage();
+    }
+  };
+
+  return cart;
+}
+
+const cart=Cart('cart-oop');
+const businessCart=Cart('cart-business');
+
+cart.loadFromStorage();
+businessCart.loadFromStorage();
+
+console.log(cart);
+console.log(businessCart);
+
+
+
+
+
+
+
+
